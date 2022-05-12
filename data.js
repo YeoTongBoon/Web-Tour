acb7aac22b3e20fd1f322aca9cd4a97bafae2a37
YeoTongBoon <105397983+YeoTongBoon@users.noreply.github.com>
2022-05-12 15:08:55 +0800

    360

---
+++ data.js
@@ -0,0 +1,138 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-drive-1",
+      "name": "Drive 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.06587348036804386,
+        "pitch": -0.027413982266175196,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3965902168991082,
+          "pitch": -0.22863330015144,
+          "rotation": 6.283185307179586,
+          "target": "1-engineering-auditorium-atrium"
+        },
+        {
+          "yaw": -0.5143461218600329,
+          "pitch": -0.21264992803145688,
+          "rotation": 0,
+          "target": "2-engineering-auditorium2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-engineering-auditorium-atrium",
+      "name": "Engineering Auditorium Atrium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.12975944338174372,
+          "pitch": 0.32358459498216874,
+          "rotation": 3.141592653589793,
+          "target": "0-drive-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-engineering-auditorium2",
+      "name": "Engineering Auditorium2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.23454829509502773,
+          "pitch": 0.23602380464515704,
+          "rotation": 3.141592653589793,
+          "target": "0-drive-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.4266285513962451,
+          "pitch": -0.6296502570901925,
+          "title": "engineerrrrr<div><br></div>",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "My project!!!",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
